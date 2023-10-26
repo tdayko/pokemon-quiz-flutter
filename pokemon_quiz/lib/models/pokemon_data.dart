4a6f4705e63348478503da908482dc4d9d3cfd0b
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-10-25 21:16:35 -0300

    banananananana

--- pokemon_quiz/lib/models/pokemon_data.dart
+++ pokemon_quiz/lib/models/pokemon_data.dart
@@ -1,12 +1,18 @@
 class PokemonData {
   final int id;
+  final int height; //altura em dm
+  final int weight; //peso em hectograma
 
-  PokemonData({required this.id});
+  PokemonData({required this.id, required this.height, required this.weight});
+
+  get heightInMeters => height / 10;
+  get weightInKg => weight / 10;
 
   factory PokemonData.fromJson(Map<String, dynamic> json) {
     return PokemonData(
       id: json['id'],
-
+      height: json['height'],
+      weight: json['weight'],
     );
   }
 }
