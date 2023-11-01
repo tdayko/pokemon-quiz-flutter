07a4f61f84e5f239274fcee8c1f986860e564a25
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-11-01 19:34:43 -0300

    contagem do quiz

--- pokemon_quiz/lib/widgets/pokemon_questions.dart
+++ pokemon_quiz/lib/widgets/pokemon_questions.dart
@@ -1,35 +1,124 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:pokedex/api/pokeapi.dart';
 import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/widgets/pokemon_questions.dart';
+import 'package:pokedex/widgets/pokemon_image.dart';
 
-class PokemonImage extends StatelessWidget {
-  const PokemonImage({required this.pokemon, super.key});
-  final Pokemon pokemon;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Pokemon? _correctPokemon;
+  List<Pokemon> _notCorrectPokemons = [];
+  int _correctAnswers = 0;
+  int _totalQuestions = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPokemon();
+  }
+
+  Future<void> _loadPokemon() async {
+    final pokemonList = await PokeAPI.getPokemonList();
+    final selectedPokemon = pokemonList[Random().nextInt(pokemonList.length)];
+    final notCorrectPokemons = <Pokemon>[];
+    for (var i = 0; i < 3; i++) {
+      final pokemon = pokemonList[Random().nextInt(pokemonList.length)];
+      if (pokemon != selectedPokemon) {
+        notCorrectPokemons.add(pokemon);
+      }
+    }
+
+    setState(() {
+      _correctPokemon = selectedPokemon;
+      _notCorrectPokemons = notCorrectPokemons;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(
-        maxHeight: 350,
-      ),
-      color: Colors.black,
-      child: Center(
-          child: Stack(
-        children: [
-          Container(
-            height: 350,
-            width: 350,
-            decoration: const BoxDecoration(
-              color: Colors.white10,
-              shape: BoxShape.circle,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Quiz de Pokémon'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Text('Acertos: $_correctAnswers/$_totalQuestions'),
+              ],
             ),
           ),
-          Image.network(
-            pokemon.image,
-            fit: BoxFit.contain,
-            alignment: Alignment.center,
-          )
         ],
-      )),
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            if (_correctPokemon != null) PokemonImage(pokemon: _correctPokemon!),
+            if (_correctPokemon != null)
+              PokemonQuiz( 
+                correctPokemon: _correctPokemon!,
+                incorrectPokemons: _notCorrectPokemons,
+                onAnswerSelected: _handleAnswerSelected,
+              ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _handleAnswerSelected(bool isCorrect) {
+    _totalQuestions++;
+
+    if (isCorrect) {
+      _correctAnswers++;
+    }
+
+    if (_totalQuestions == 10) {
+      _showResults();
+    } else {
+      _loadPokemon();
+    }
+  }
+
+  void _showResults() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Resultado do Quiz'),
+          content: Text('Acertos: $_correctAnswers de $_totalQuestions'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                // Reiniciar o jogo se necessário
+                _resetQuiz();
+              },
+              child: Text('Fechar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _correctAnswers = 0;
+      _totalQuestions = 0;
+      _correctPokemon = null;
+      _notCorrectPokemons.clear();
+    });
+
+    _loadPokemon();
   }
 }
